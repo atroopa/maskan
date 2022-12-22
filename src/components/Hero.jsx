dc8d5cc29a0f88a3b03d852964b272020a08f849
Atroopa <atroopa@gmail.com>
2022-12-22 19:27:58 +0330

    navbar finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-orange-100'>
         <div className='md:right-[10%] max-w-[1100px] m-auto absolute p-4'>
           <p>همه چیز محیاست</p>
-          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>شما لایق یک زندگی آرام هستید</h1>
-          <p className='max-w-[660px] drop-shadow-2xl py-2 text-xl'>املاک دهکده با ده سال سابقه درخشان آماده است تا بهترین ملک را به توجه به نیاز شما تهیه کند.</p>
-          <button className='bg-white text-black font-bold '> بازدید </button>
+          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl '>شما لایق یک زندگی آرام هستید</h1>
+          <p className='max-w-[440px] drop-shadow-2xl py-6 text-xl'>املاک دهکده با ده سال سابقه درخشان آماده است تا بهترین ملک را به توجه به نیاز شما تهیه کند. سرمایه گذاری در ملک تا به حال کسی را نا امید نکرده است</p>
+          <button className= ' bg-orange-100 text-black font-bold '> بازدید </button>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {HiMenuAlt3} from 'react-icons/hi';
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
-    <div>
-        <h1>Dehkadeh</h1>
+    <div className='absolute w-full flex justify-between p-4 items-center'>
+      <h1 className='text-orange-100 font-bold text-2xl z-20'>Dehkadeh</h1>
+      <HiMenuAlt3 onClick={handleNav} className='text-orange-100 z-20 cursor-pointer' size={25}/>
+      <div 
+          className={
+            nav
+            ? 'fixed text-orange-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10'
+            : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10'
+          }
+      >
+        <ul className='flex flex-col fixed w-full h-full items-center justify-center'>
+          <li className='font-bold text-3xl p-8'> خانه </li>
+          <li className='font-bold text-3xl p-8'> آپارتمان </li>
+          <li className='font-bold text-3xl p-8'> زمین </li>
+          <li className='font-bold text-3xl p-8'> مشارکت </li>
+          <li className='font-bold text-3xl p-8'> درباره ما </li>
+        </ul>
+      </div>
     </div>
   )
 }
