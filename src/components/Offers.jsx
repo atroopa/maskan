75c5eacea7b1721a4d13f30a06c37e426b23e57b
Atroopa <atroopa@gmail.com>
2022-12-23 17:56:26 +0330

    office is persian

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Offers = () => {
   return (
     <div className='max-w-[990px] m-auto px-4 py-12 flex flex-wrap justify-between'>
-        <p className='text-lg font-bold text-gray-700'>Bookking incentive</p>
-        <p className='text-lg font-bold text-gray-700'>Competitive Rates</p>
-        <p className='text-lg font-bold text-gray-700'>Book With Flexibility</p>
-        <p className='text-lg font-bold text-gray-700'>Bokking incentive</p>
+        <p className='text-lg font-bold text-gray-700'>شرکت های موفق سرمایه گذاری</p>
+        <p className='text-lg font-bold text-gray-700'>مشاوره عمیق و آینده نگرانه</p>
+        <p className='text-lg font-bold text-gray-700'>معرفی درخشان ترین سازنده ها</p>
+        <p className='text-lg font-bold text-gray-700'>قوانین مبادلات ملکی </p>
     </div>
   )
 }
