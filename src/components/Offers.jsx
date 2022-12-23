7f946554555202a7c2e96e9ea7467bdc17c36f9c
Atroopa <atroopa@gmail.com>
2022-12-23 17:52:03 +0330

    finish offers

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Offers = () => {
   return (
-    <div>Offers</div>
+    <div className='max-w-[990px] m-auto px-4 py-12 flex flex-wrap justify-between'>
+        <p className='text-lg font-bold text-gray-700'>Bookking incentive</p>
+        <p className='text-lg font-bold text-gray-700'>Competitive Rates</p>
+        <p className='text-lg font-bold text-gray-700'>Book With Flexibility</p>
+        <p className='text-lg font-bold text-gray-700'>Bokking incentive</p>
+    </div>
   )
 }
 
-export default Offers
+export default Offers;
