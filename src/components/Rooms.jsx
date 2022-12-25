4cd45dac8952b108d38cd59fe185520d01919dc1
Atroopa <atroopa@gmail.com>
2022-12-25 11:53:18 +0330

    add rooms jsx

---
+++ src/components/Rooms.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Rooms = () => {
+  return (
+    <div>
+        <div>
+            <h3>Fine Interior Rooms</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Iste voluptate dolorem, doloremque similique quae nulla!\
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Rooms
